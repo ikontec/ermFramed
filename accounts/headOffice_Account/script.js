84888381ea0eb2d17bc469d05f4c0de68ed70c93
ikontec <ikontec25@gmail.com>
2025-06-08 05:34:20 -0700

    hhhhhhh

---
+++ accounts/headOffice_Account/script.js
@@ -0,0 +1,15 @@
+        // Toggle menu and arrow icon
+        const menuButton = document.getElementById('menuToggle');
+        const menuIcon = document.getElementById('menuIcon');
+        const navList = document.querySelector('.nav ul');
+
+        menuButton.addEventListener('click', function() {
+            navList.classList.toggle('show');
+            if (navList.classList.contains('show')) {
+                menuIcon.classList.remove('fa-chevron-down');
+                menuIcon.classList.add('fa-chevron-up');
+            } else {
+                menuIcon.classList.remove('fa-chevron-up');
+                menuIcon.classList.add('fa-chevron-down');
+            }
+        });
